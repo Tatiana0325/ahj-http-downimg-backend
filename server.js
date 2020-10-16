b283a4f9e39c60d109a93a0aa5a81427cdeaf7cb
Tatiana Krasnova <krasnova-tatyana92@mail.ru>
2020-10-16 20:43:47 +0300

    update server.js

--- server.js
+++ server.js
@@ -10,6 +10,36 @@ const app = new Koa();
 const publ = path.join(__dirname, "/public");
 const koaStatic = require("koa-static");
 
+app.use(async (ctx, next) => {
+  const origin = ctx.request.get('Origin');
+  if (!origin) {
+      return await next();
+  }
+
+  const headers = { 'Access-Control-Allow-Origin': '*', };
+  if (ctx.request.method !== 'OPTIONS') {
+      ctx.response.set({...headers});
+      try {
+          return await next();
+      } catch (e) {
+          e.headers = {...e.headers, ...headers};
+          throw e;
+      }
+  }
+
+  if (ctx.request.get('Access-Control-Request-Method')) {
+      ctx.response.set({
+      ...headers,
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH',
+  });
+      if (ctx.request.get('Access-Control-Request-Headers')) {
+          ctx.response.set('Access-Control-Allow-Headers',
+          ctx.request.get('Access-Control-Allow-Request-Headers'));
+      }
+      ctx.response.status = 204; // No content
+      }
+});
+
 app.use(
   koaBody({
     urlencoded: true,
